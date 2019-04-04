30e69b1affdbd33ca777a41b2bcf6503345d8404
ImaneBoualaoui <imane.92@live.fr>
2019-04-04 20:10:00 +0200

    mot de passe invisible

--- lib/02_password.rb
+++ lib/02_password.rb
@@ -1,37 +1,49 @@
 def signup
 	puts "Saissisez un mot de passe :"
 	print ">"
-	password = gets.chomp
+	#password = gets.chomp
+
+	#Pour que le mot de passe ne soit pas visible
+	require 'io/console'
+	password = STDIN.noecho(&:gets).chomp
 	return password	
 end
 
 def login(password)
 	puts "Saissisez à nouveau votre mot de passe :"
 	print ">"
-	password_again = gets.chomp
+	#password_again = gets.chomp
 
-	if password_again != password
-		while password_again != password
-			puts "le mot de passe ne correspond pas, veuillez le saissir à nouveau :"
-			print ">"
-			password_again = gets.chomp
-		end
+	#Pour que le mot de passe ne soit pas visible
+	require 'io/console'
+	password_again = STDIN.noecho(&:gets).chomp
+
+	while password_again != password 
+		puts "le mot de passe ne correspond pas, veuillez le saissir à nouveau :"
+		print ">"
+		#password_again = gets.chomp
 		
-	end
-	else
-		puts "le mot de passe est correct, vous allez être redirigez vers la page d'acceuil"
-	end	
+		#Pour que le mot de passe ne soit pas visible
+		require 'io/console'
+		password_again = STDIN.noecho(&:gets).chomp
 
+	end
 	return password_again
 end
 
-#def method_name
-	
-#end
+def welcome_scree(password, password_again)
+	if password == password_again
+		puts "Accès autorisé ! Bienvenue !"
+		puts "Vous avez accès à tous les messages !"
+		
+	end
+end
 
 def perform
 	password = signup
-	login(password)
+	password_again = login(password)
+
+	welcome_scree(password, password_again)
 end
 
 perform
